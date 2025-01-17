ed9c358d4c883ad14306532c28b87553ab13f173
ShadowShahriar <shadowshahriar.dev@gmail.com>
2025-01-18 04:51:59 +0600

    Solve task 1 and 2

---
+++ practice-01/02-unicode-output/Emoji.java
@@ -0,0 +1,8 @@
+class Emoji {
+	public static void main(String[] args) {
+		String s1 = "Change the active codepage by running \"chcp 65001\" in the terminal.";
+		String s2 = "Then you should see these emojis: ✨😁🎉";
+		System.out.println(s1);
+		System.out.println(s2);
+	}
+}
